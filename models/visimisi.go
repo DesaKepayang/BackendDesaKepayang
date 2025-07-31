0a39edcbf2f4e9fe1ded906c4dc0756faa690203
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-07-31 11:52:04 +0700

    ganti panjang visi dan misi

--- models/visimisi.go
+++ models/visimisi.go
@@ -2,8 +2,8 @@ package models
 
 type VisiMisi struct {
 	IDVisiMisi uint   `gorm:"column:id_visimisi;primaryKey;autoIncrement"`
-	Visi       string `gorm:"type:varchar(10);not null"`
-	Misi       string `gorm:"type:varchar(10);not null"`
+	Visi       string `gorm:"type:varchar(855);not null"`
+	Misi       string `gorm:"type:varchar(855);not null"`
 }
 
 func (VisiMisi) TableName() string {
