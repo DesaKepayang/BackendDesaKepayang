f2b17d2276cb7a9928cefb341313a6d6cc021213
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-07-31 16:17:23 +0700

    perbaiki models penduduk

--- models/penduduk.go
+++ models/penduduk.go
@@ -1,13 +1,15 @@
 package models
 
 type DataPenduduk struct {
-	IDPenduduk uint   `gorm:"column:id_penduduk;primaryKey;autoIncrement"`
-	IDRTRW     uint   `gorm:"column:id_rtrw;not null"` // foreign key
-	Nama       string `gorm:"type:varchar(100);not null"`
-	Agama      string `gorm:"type:varchar(50);not null"`
-	Gender     string `gorm:"type:varchar(10);not null"`
+	IDPenduduk uint `gorm:"column:id_penduduk;primaryKey;autoIncrement"`
+	IDRTRW     uint `gorm:"column:id_rtrw;not null"`
 
-	RTRW RTRW `gorm:"foreignKey:IDRTRW;references:IDRTRW"` // relasi ke tabel rt_rw
+	Nama   string `gorm:"type:varchar(100);not null"`
+	Agama  string `gorm:"type:varchar(50);not null"`
+	Gender string `gorm:"type:varchar(10);not null"`
+
+	// Relasi dengan tabel rt_rw
+	RTRW RTRW `gorm:"foreignKey:IDRTRW;references:IDRTRW;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 }
 
 func (DataPenduduk) TableName() string {
