de8ea98d9e490f42bbeefc43d07beb7c203890b7
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-07-31 12:00:53 +0700

    tambahkan middleware

--- routes/visimisi_route.go
+++ routes/visimisi_route.go
@@ -2,16 +2,21 @@ package routes
 
 import (
 	"desa-kepayang-backend/controllers"
+	"desa-kepayang-backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func VisiMisiRoutes(r *gin.Engine) {
-	route := r.Group("/visimisi")
+	// Route GET (publik)
+	r.GET("/visimisi", controllers.GetAllVisiMisi)
+
+	// Route yang dilindungi dengan middleware (POST, PUT, DELETE)
+	protected := r.Group("/visimisi")
+	protected.Use(middleware.AuthMiddleware())
 	{
-		route.GET("/", controllers.GetAllVisiMisi)
-		route.POST("/", controllers.CreateVisiMisi)
-		route.PUT("/:id", controllers.UpdateVisiMisi)
-		route.DELETE("/:id", controllers.DeleteVisiMisi)
+		protected.POST("/", controllers.CreateVisiMisi)
+		protected.PUT("/:id", controllers.UpdateVisiMisi)
+		protected.DELETE("/:id", controllers.DeleteVisiMisi)
 	}
 }
