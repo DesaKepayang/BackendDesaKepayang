7a0fe9388dc923c4748575cff566c4c991ea3481
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-08-02 13:37:14 +0700

    memperbaiki komentar

--- models/komentar.go
+++ models/komentar.go
@@ -1,11 +1,11 @@
 package models
 
 type Komentar struct {
-	IDKomentar uint   `gorm:"column:id_komentar;primaryKey;autoIncrement"`
-	Nama       string `gorm:"column:nama;type:varchar(100);not null"`
-	Email      string `gorm:"column:email;type:varchar(100);not null"`
-	NoHP       string `gorm:"column:no_hp;type:varchar(20);not null"`
-	Komentar   string `gorm:"column:komentar;type:text;not null"`
+	IDKomentar uint   `gorm:"column:id_komentar;primaryKey;autoIncrement" json:"id_komentar"`
+	Nama       string `gorm:"column:nama;type:varchar(100);not null" json:"nama"`
+	Email      string `gorm:"column:email;type:varchar(100);not null" json:"email"`
+	NoHP       string `gorm:"column:no_hp;type:varchar(20);not null" json:"no_hp"`
+	Komentar   string `gorm:"column:komentar;type:text;not null" json:"komentar"`
 }
 
 func (Komentar) TableName() string {
