5facba348bcf3e66226e05fad7e3bdbc157822e9
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-08-05 14:12:42 +0700

    mencoba drop kolom foto di tabel visi misi

--- models/visimisi.go
+++ models/visimisi.go
@@ -1,13 +1,18 @@
 package models
 
+import "gorm.io/gorm"
+
 type VisiMisi struct {
 	IDVisiMisi uint   `gorm:"column:id_visimisi;primaryKey;autoIncrement"`
 	Visi       string `gorm:"type:varchar(855);not null"`
 	Misi       string `gorm:"type:varchar(855);not null"`
 }
 
-//hilangkan foto pada tabel visi misi
-
 func (VisiMisi) TableName() string {
 	return "visi_misi"
+}
+
+// Fungsi untuk drop kolom foto jika masih ada di DB
+func DropFotoColumn(db *gorm.DB) {
+	db.Migrator().DropColumn(&VisiMisi{}, "foto")
 }
