d91a367876e1fbe5e6cace28004a968084a2b1c3
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-08-06 14:37:45 +0700

    merubah models admin

--- models/admin.go
+++ models/admin.go
@@ -2,9 +2,9 @@ package models
 
 type Admin struct {
 	ID       uint   `gorm:"column:id_admin;primaryKey;autoIncrement"`
-	Username string `gorm:"type:varchar(100);not null"`
-	Password string `gorm:"type:varchar(255);not null"`
-	Role     string `gorm:"type:varchar(50);not null"`
+	Username string `gorm:"column:username;type:varchar(100);not null"`
+	Password string `gorm:"column:password;type:varchar(255);not null"`
+	Role     string `gorm:"column:role;type:varchar(50);not null"`
 }
 
 func (Admin) TableName() string {
