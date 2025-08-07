f93e801feff8f34244163f5c3b5e4f71cf12646b
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-08-07 11:01:12 +0700

    memperbaiki modes admin

--- models/admin.go
+++ models/admin.go
@@ -1,10 +1,10 @@
 package models
 
 type Admin struct {
-	ID       uint   `gorm:"column:id_admin;primaryKey;autoIncrement"`
-	Username string `gorm:"column:username;type:varchar(100);not null"`
-	Password string `gorm:"column:password;type:varchar(255);not null"`
-	Role     string `gorm:"column:role;type:varchar(50);not null"`
+	ID       uint   `gorm:"column:id_admin;primaryKey;autoIncrement" json:"id_admin"`
+	Username string `gorm:"column:username;type:varchar(100);not null" json:"username"`
+	Password string `gorm:"column:password;type:varchar(255);not null" json:"-"`
+	Role     string `gorm:"column:role;type:varchar(50);not null" json:"role"`
 }
 
 func (Admin) TableName() string {
