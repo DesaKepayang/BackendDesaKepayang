1d0e970c9a1a20ac7dc468f3d4280e886ebc5eaa
DesaKepayang <kepayangbackenddesa@gmail.com>
2025-08-13 12:52:40 +0700

    memperbaiki tabel info desa

--- models/info_desa.go
+++ models/info_desa.go
@@ -1,17 +1,17 @@
 package models
 
 type InfoDesa struct {
-	IDInfo        uint `gorm:"column:id_info;primaryKey;autoIncrement"`
-	JumlahKK      int  `gorm:"column:jumlahkk;not null"`
-	TotalPenduduk int  `gorm:"column:total_penduduk;not null"`
-	LakiLaki      int  `gorm:"column:laki_laki;not null"`
-	Perempuan     int  `gorm:"column:perempuan;not null"`
-	Islam         int  `gorm:"column:islam;not null"`
-	Buddha        int  `gorm:"column:buddha;not null"`
-	Kristen       int  `gorm:"column:kristen;not null"`
-	Katolik       int  `gorm:"column:katolik;not null"`
-	Konghucu      int  `gorm:"column:konghucu;not null"`
-	LainLain      int  `gorm:"column:lain_lain;not null"`
+	IDInfo        uint `gorm:"column:id_info;primaryKey;autoIncrement" json:"id_info"`
+	JumlahKK      int  `gorm:"column:jumlahkk;not null" json:"jumlahkk"`
+	TotalPenduduk int  `gorm:"column:total_penduduk;not null" json:"total_penduduk"`
+	LakiLaki      int  `gorm:"column:laki_laki;not null" json:"laki_laki"`
+	Perempuan     int  `gorm:"column:perempuan;not null" json:"perempuan"`
+	Islam         int  `gorm:"column:islam;not null" json:"islam"`
+	Buddha        int  `gorm:"column:buddha;not null" json:"buddha"`
+	Kristen       int  `gorm:"column:kristen;not null" json:"kristen"`
+	Katolik       int  `gorm:"column:katolik;not null" json:"katolik"`
+	Konghucu      int  `gorm:"column:konghucu;not null" json:"konghucu"`
+	LainLain      int  `gorm:"column:lain_lain;not null" json:"lain_lain"`
 }
 
 func (InfoDesa) TableName() string {
